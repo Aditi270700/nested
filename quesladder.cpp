d0f4fff44eaeb4c1ebf0235890eda77d049de2de
aditi saudagar <aditisaudagar17@gmail.com>
2024-09-19 17:42:34 +0530

    commit

---
+++ quesladder.cpp
@@ -0,0 +1,29 @@
+//to generate electricity bill
+// slab1 = 0-100 @3Rs
+// slab2 = 101-200 @5Rs
+// slab3 = 201-500 @7Rs
+// slab4 = above 500 @10Rs
+// slab5 = less than zero invalid reading
+#include <iostream>
+using namespace std;
+int main (){
+    int bill;
+    int reading;
+    cout << "Enter the reading" << "\n";
+    cin >> reading;
+    if(reading>=1 && reading<=100){
+cout << "bill amount" << reading*3 << "\n";
+    }
+     else if(reading>=101 && reading<=200){
+cout << "bill amount" << reading*5 << "\n";
+    }
+     else if(reading>=201 && reading<=500){
+      cout << "bill amount"  << reading*7 << "\n" ;
+    }
+    else if(reading>500){
+cout<< "bill amount" << reading*10 << "\n";
+    }
+    else{
+        cout << "invalid reading" << "\n";
+    }
+}
