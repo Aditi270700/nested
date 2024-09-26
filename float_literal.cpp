b80a10fbb02102af370ea5432662616d61536960
aditi saudagar <aditisaudagar17@gmail.com>
2024-09-26 21:56:35 +0530

    commit

---
+++ float_literal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+float f,c,e=10 ;
+f = 21.5;
+cout << f << "\n";
+c = 21.5e2;
+cout << c;
+}
---
+++ square_loop.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int d = 1;
+    do{
+        cout << d*d << "\n";
+d++;
+    }while(d<=10);
+return 0;
+}
